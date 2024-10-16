037258bf72d4458ad5678264b67ac14caee594d1
ZaidanFathan <Goldhair243@gmail.com>
2024-10-17 05:41:40 +0700

    Assignment 2

---
+++ Tugas-2/Casting.java
@@ -0,0 +1,18 @@
+public class Casting {
+    public static void main(String[] args) {
+        
+        // Casting int to double
+        int myInt = 15;
+        double myDouble = myInt;
+
+        System.out.println("myInt Value : " + myInt);
+        System.out.println("myDouble Value : " + myDouble);
+
+        // Casting double to int
+        double value1 = 99.4d;
+        int value2 = (int) value1;
+
+        System.out.println("Double value : " + value1);
+        System.out.println("Integer value : " + value2);
+    }
+}
---
+++ Tugas-2/comment.java
@@ -0,0 +1,10 @@
+public class comment {
+    public static void main(String[] args) {
+        // Komentar
+        System.out.println("Test"); // Komentar
+        /* 
+         * Komentar 1
+         * Komentar 2
+         */
+    }
+}
